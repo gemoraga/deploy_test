f6afc6d7497416de58128b3dc17df42a5bbd837a
gemoraga <gemoraga@uc.cl>
2016-12-26 11:40:25 -0300

    agregada gem pg

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ group :development, :test do
   gem 'byebug', platform: :mri
 end
 
+gem 'pg'
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "deploy_test"
 set :repo_url, "git@github.com:gemoraga/deploy_test.git"
 
-set :deploy_to, '/home/deploy/my_app_name'
+set :deploy_to, '/home/deploy/deploy_test'
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
