a2f49d313452bae764c09982f3298419fbb1c7d2
jfcumsille <jfcumsille@uc.cl>
2016-12-28 10:55:26 -0300

    config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,22 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "deploy_test_#{Rails.env}"
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: 'gmail.com',
+  user_name: "contacto.prodelco@gmail.com",
+  password: "prodelco123",
+  authentication: :plain,
+  enable_starttls_auto: true
+  }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
